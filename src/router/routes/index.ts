b55e77e37a748262ccfb989a5723fdac327144b2
hjp <x>
2024-12-16 08:51:52 +0800

    refactor(login): 更改login页面

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -54,7 +54,7 @@ export const constantRoutes: AuthRoute.Route[] = [
             {
                 path: '/login',
                 name: 'login',
-                component: () => import('@/views/_builtin/test/test.vue'),
+                component: views.login,
                 meta: { title: '登录', allowAnonymous: true },
             },
             ...ERROR_ROUTES,
