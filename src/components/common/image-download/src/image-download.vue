7f2a01a23881768494a130e73c1ba24bc97e866a
xhx <hx.xu@hongzhengtech.com>
2024-12-30 14:54:14 +0800

    会议二维码下载

--- src/components/common/image-download/src/image-download.vue
+++ src/components/common/image-download/src/image-download.vue
@@ -10,7 +10,7 @@ const props = defineProps({
         type: Object,
         default: () => {},
     },
-    needDownLoad: {
+    closeDownload: {
         type: Boolean,
         default: false,
     },
@@ -22,16 +22,19 @@ const props = defineProps({
 
 // 添加下载图标
 function onAddDownSet() {
-    nextTick(() => {
-        const _this = document.getElementsByClassName('el-image-viewer__wrapper')[0]
-        const btnGroup = _this.getElementsByClassName('el-image-viewer__actions__inner')[0]
-        const downloadHtml = `<i class="el-icon down-load"><svg data-v-d2e47025="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M160 832h704a32 32 0 1 1 0 64H160a32 32 0 1 1 0-64m384-253.696 236.288-236.352 45.248 45.248L508.8 704 192 387.2l45.248-45.248L480 584.704V128h64z"></path></svg></i>`
-        btnGroup.insertAdjacentHTML('beforeend', downloadHtml)
-        const downLoadBtn = btnGroup.getElementsByClassName('down-load')[0]
-        downLoadBtn.addEventListener('click', () => {
-            downloadImage()
+    if (!props.closeDownload) {
+        console.log('1')
+        nextTick(() => {
+            const _this = document.getElementsByClassName('el-image-viewer__wrapper')[0]
+            const btnGroup = _this.getElementsByClassName('el-image-viewer__actions__inner')[0]
+            const downloadHtml = `<i class="el-icon down-load"><svg data-v-d2e47025="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M160 832h704a32 32 0 1 1 0 64H160a32 32 0 1 1 0-64m384-253.696 236.288-236.352 45.248 45.248L508.8 704 192 387.2l45.248-45.248L480 584.704V128h64z"></path></svg></i>`
+            btnGroup.insertAdjacentHTML('beforeend', downloadHtml)
+            const downLoadBtn = btnGroup.getElementsByClassName('down-load')[0]
+            downLoadBtn.addEventListener('click', () => {
+                downloadImage()
+            })
         })
-    })
+    }
 }
 
 function downloadImage() {
